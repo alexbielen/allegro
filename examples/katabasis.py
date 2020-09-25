c8a6ee0be7b881f1c1083dbc91e15021e5836689
Alex Hendrie Bielen <alexhendriebielen@gmail.com>
2020-09-25 09:58:13 -0400

    Add constraint engine and an example.
    See: https://developers.google.com/optimization
    
    Why: Adding a constraint engine to do musical constraint programming.
    Since constraint programming can be resource intensive, I am relying on
    Google's OR-Tools (Operations Research Tools) which has a built-in
    constraint solver. The OR-Tools API is a little too broad for musical
    applications (at least my own applications!) so I am creating a facade
    to make it easier. Additionally, I am providing a few helper functions
    and making instantiating a solver much simpler.
    
    Additionally I added an example of how to use the constraint library in
    the `examples` directoy. This example is from a piece that I am
    currently working on, that I is constrainted to palindromic 12-tone
    chords (in fixed pitch space).
    
    How: Adding a new file in the allegro module called constraints.py. This
    file has a class `Problem` that provides an API for modelling and
    solving constraint satisfaction problems. Google's OR-Tools is very
    broad, but I provided access to the `model` object in to keep things
    flexible. I added tests.
    
    Tags: constraints, or-tools

---
+++ examples/katabasis.py
@@ -0,0 +1,42 @@
+from allegro.constraints import Problem, unique_pitch_classes
+from allegro.iterables import current_with_next
+
+
+def katabasis():
+    p = Problem()
+
+    num_notes = 12
+    lower_bound = 22
+    upper_bound = 108
+
+    notes = [p.add_constant(21)]
+    for i in range(num_notes - 1):
+        var = p.add_variable(lower_bound, upper_bound, f"n{i}")
+        notes.append(var)
+
+    intervals = []
+    for i in range(num_notes - 1):
+        var = p.add_variable(1, 7, f"i{i}")
+        intervals.append(var)
+
+    p.add_all_different_constraint(notes)
+
+    # variables should ascend
+    for i, (current, next_) in enumerate(current_with_next(notes)):
+        if next_:
+            p.add_constraint(current + intervals[i] == next_)
+
+    p.add_constraint(intervals[0] == intervals[10])
+    p.add_constraint(intervals[1] == intervals[9])
+    p.add_constraint(intervals[2] == intervals[8])
+    p.add_constraint(intervals[3] == intervals[7])
+    p.add_constraint(intervals[4] == intervals[6])
+
+    p.add_filter(unique_pitch_classes)
+
+    solutions = p.solve(show_variables=notes)
+    return solutions
+
+
+if __name__ == "__main__":
+    print(katabasis())
