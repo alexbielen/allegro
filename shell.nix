53733b5ed68e2d70063e7e2b1a93805424d95214
Alex Hendrie Bielen <alexhendriebielen@gmail.com>
2023-07-14 22:42:31 -0400

    Update various dependencies
    Why: Trying to create a reproducible dev environment using Nix shell and
    poetry.
    
    How: I updated the shell.nix file to use a direct tarball instead of a
    nix channel. It appears that nix-channels are going out of favor and
    they have lots of bugs and reproducibility issues. So, I switched to the
    tarball and updated to python 3.11. I added back ortools and rtmidi as
    well.
    
    Tags: dev, environment, nix

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
-{ pkgs ? import <nixpkgs> { } }:
+{ pkgs ? import (fetchTarball
+  "https://github.com/NixOS/nixpkgs/archive/refs/tags/23.05.tar.gz") { } }:
 
 pkgs.mkShell {
-
-  buildInputs = [ pkgs.python310 pkgs.poetry pkgs.nixfmt ];
-
+  buildInputs =
+    [ pkgs.python311 pkgs.poetry pkgs.nixfmt pkgs.lolcat pkgs.which ];
 }
